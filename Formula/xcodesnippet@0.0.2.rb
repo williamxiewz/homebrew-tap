bc4e1d21d97a60a1e9e59112bb13a16df33324ed
WilliamXie <4928606+williamxiewz@users.noreply.github.com>
2023-01-09 15:13:08 +0800

    Create xcodesnippet@0.0.2.rb

---
+++ Formula/xcodesnippet@0.0.2.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Xcodesnippet < Formula
+  desc "xcodesnippet Tool"
+  homepage ""
+  url "https://github.com/williamxiewz/xcodesnippet/releases/download/0.0.2/xcodesnippet_0.0.2.tar.gz"
+  sha256 "b83abaa27d6c8387e57d6310aa71a3fe600fde4c4dcb6f613b241c03ff8717e1"
+  license "MIT"
+  version "0.0.2"
+  
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "xcodesnippet"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test xcodesnippet`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    #  system "false"
+  end
+end
