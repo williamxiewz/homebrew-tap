cb26cb8fd0e40120189693a649c70a3e475d16b9
WilliamXie <4928606+williamxiewz@users.noreply.github.com>
2023-01-09 15:26:47 +0800

    Update xcodesnippet.rb

--- Formula/xcodesnippet.rb
+++ Formula/xcodesnippet.rb
@@ -7,7 +7,6 @@ class Xcodesnippet < Formula
   url "https://github.com/williamxiewz/xcodesnippet/releases/download/0.0.2/xcodesnippet_0.0.2.tar.gz"
   sha256 "2d52534c311130125d41d454ff97ceae3bcfe5d10ed8b18f0a3694dc77506267"
   license "MIT"
-  version "0.0.2"
   
   # depends_on "cmake" => :build
 
